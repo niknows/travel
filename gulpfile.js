acdb659360df0bfee771c3a007b2fbc7b0c21856
Nicolas <ngaribaldif@gmail.com>
2018-09-15 23:00:15 -0300

    moved @import

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task("styles", function(){
   //get info from file
 	return gulp.src('./app/assets/styles/style.css')
   //applies postcss filters
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport , cssvars, nested, autoprefixer])) //postcss modules
   //create a new file containing filtered data
   .pipe(gulp.dest('./app/temp/styles'));
 });
