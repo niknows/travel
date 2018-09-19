3bc8b916806975d40c1462474f52736ffd5043d7
Nicolas <ngaribaldif@gmail.com>
2018-09-18 23:10:37 -0300

    browser-sync setup

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'), //css variables support
     nested = require('postcss-nested'), //enables nested css syntax
     cssImport = require('postcss-import'), //watches for @import flag and injects the model content inside the main css file
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync').create();
 //gulp needs a default task to start working
 //task name + what this task does
 gulp.task('default',function(){
@@ -28,12 +28,23 @@ gulp.task("styles", function(){
 
 //watches for changes in index.html
 gulp.task('watch', function(){
+
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
 	watch('./app/index.html', function(){
-		gulp.start('html');
+		browserSync.reload();
 	});
 //watches for any css file changes from any directory within /styles
 	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
+		gulp.start('cssInject');
+  });
 });
 
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/style.css')
+  .pipe(browserSync.stream());
 });
