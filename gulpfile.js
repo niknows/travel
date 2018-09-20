accd15e406169223aafff39e4488bc1b9f49ef0a
Nicolas <ngaribaldif@gmail.com>
2018-09-19 23:06:56 -0300

    gulp styles

--- gulpfile.js
+++ gulpfile.js
@@ -17,14 +17,6 @@ gulp.task("html",function(){
 	console.log("Hello world");
 });
 
-gulp.task("styles", function(){
-  //get info from file
-	return gulp.src('./app/assets/styles/style.css')
-  //applies postcss filters
-  .pipe(postcss([cssImport , cssvars, nested, autoprefixer])) //postcss modules
-  //create a new file containing filtered data
-  .pipe(gulp.dest('./app/temp/styles'));
-});
 
 //watches for changes in index.html
 gulp.task('watch', function(){
