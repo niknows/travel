75be7fcfeaa4bdceba0e7955247cf2038b285845
Nicolas <ngaribaldif@gmail.com>
2018-09-19 23:15:35 -0300

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,6 @@
 //selected modules
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create();
-//gulp needs a default task to start working
-//task name + what this task does
-gulp.task('default',function(){
-	console.log("Hello, this is a  gulp task");
-});
-
-gulp.task("html",function(){
-	console.log("Hello world");
-});
 
 gulp.task('cssInject', ['styles'], function(){
   return gulp.src('./app/temp/styles/style.css')
